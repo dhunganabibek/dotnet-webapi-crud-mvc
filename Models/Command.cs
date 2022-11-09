83998b7fa7f1fd3a223b3f12fbc3f7ee274dbcd3
Bibek Dhungana <bibek.dhungana@hcss.com>
2022-11-08 19:13:48 -0600

    Model added

---
+++ Models/Command.cs
@@ -0,0 +1,12 @@
+namespace Commander.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }         
+        public String HowTo { get; set; }
+
+        public String Line { get; set; }
+
+        public String Platform { get; set; }
+    }
+}
